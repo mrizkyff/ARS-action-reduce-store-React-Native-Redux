fac6aa5f36df86bf4e15508e61b1ae08e8618801
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-27 15:30:31 +0700

    redux success

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import Counter from './src/counter'
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+
+const initialState = {
+  count: 10
+}
+
+const reducer = (state = initialState, action) => {
+  // console.log(action);
+  const payload = action.payload || 1
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + payload
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - payload
+      }
+    default:
+      break;
+  }
+  return state
+}
+
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Counter/>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Counter />
+        </View>
+      </Provider>
     )
   }
 }
--- src/counter/index.js
+++ src/counter/index.js
@@ -1,45 +1,50 @@
-import React, { useEffect } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useEffect, Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
 import { useState } from 'react';
-
-
-const Counter = () => {
-
-
-    const Tombol = (props) => {
-        return (
-            <TouchableOpacity>
-                <Text style={{ fontSize: 25, backgroundColor: props.warna }} onPress={props.onPress}>{props.judul}</Text>
-            </TouchableOpacity>
-        )
-    }
-
-    const [jumlah, setJumlah] = useState(0);
-
-    const increment = () => {
-        // Alert.alert('Halo ian, ini adalah fungsi increment!')
-        setJumlah(jumlah + 1)
-    }
-
-    const decrement = () => {
-        // Alert.alert('Halo ian, ini adalah fungsi increment!')
-        setJumlah(jumlah - 1)
-    }
-
+import { connect } from "react-redux";
+const Tombol = (props) => {
     return (
         <View>
-            <View style={styles.container}>
-                <Text style={{ fontSize: 30 }}>Basic Redux</Text>
-                <Text style={styles.text}>Hello World!</Text>
-                <Text style={{ fontSize: 25 }}>{jumlah}</Text>
-                <View style={{ flexDirection: 'row' }}>
-                    <Tombol warna='green' judul='INCREMENT 1' onPress={() => increment()}/>
-                    <Tombol warna='red' judul='DECREMENT 1' onPress={() => decrement()}/>
-                </View>
-            </View>
+            <TouchableOpacity>
+                <Text style={{ fontSize: 25, backgroundColor: props.warna, padding: 10, marginRight: 5 , marginBottom:10}} onPress={props.onPress}>{props.judul}</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+class Counter extends Component {
+    render() {
+        return (
+            <View>
+                <View style={styles.container}>
+                    <Text style={{ fontSize: 30 }}>Basic Redux</Text>
+                    <Text style={styles.text}>Hello World!</Text>
+                    <Text style={{ fontSize: 25 }}>{this.props.count}</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Tombol warna='green' judul='INCREMENT 1' onPress={this.props.increment} />
+                        <Tombol warna='red' judul='DECREMENT 1' onPress={this.props.decrement} />
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Tombol warna='blue' judul='INCREMENT 5' onPress={this.props.increment5} />
+                        <Tombol warna='orange' judul='DECREMENT 5' onPress={this.props.decrement5} />
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+
+const mapStateToProps = state => ({
+    count: state.count
+})
+
+const mapDispatchToProps = dispatch => ({
+    increment : () => dispatch({type: 'INCREMENT'}),
+    decrement : () => dispatch({type: 'DECREMENT'}),
+    increment5 : () => dispatch({type: 'INCREMENT', payload: 5}),
+    decrement5 : () => dispatch({type: 'DECREMENT', payload: 5})
+})
 
 
 const styles = StyleSheet.create({
@@ -54,4 +59,4 @@ const styles = StyleSheet.create({
 })
 
 
-export default Counter
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
